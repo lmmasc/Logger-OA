c3db9543f8f67aa66bbdadfd8146819235379f5b
lmmasc <lmmasc51@gmail.com>
2025-09-03 10:00:27 -0500

    Refactor: prefijo menu_ en claves de traducción de menús y corrección de indentación en menu_bar.py

--- src/interface_adapters/ui/menu_bar.py
+++ src/interface_adapters/ui/menu_bar.py
@@ -47,19 +47,21 @@ class MainMenuBar(QMenuBar):
 
     def _create_menus(self):
         # Menú Archivo
-        self.file_menu = QMenu(translation_service.tr("file_menu"), self)
-        self.open_folder_action = QAction(translation_service.tr("open_folder"), self)
+        self.file_menu = QMenu(translation_service.tr("menu_file_menu"), self)
+        self.open_folder_action = QAction(
+            translation_service.tr("menu_open_folder"), self
+        )
         self.file_menu.addAction(self.open_folder_action)
-        self.exit_action = QAction(translation_service.tr("exit"), self)
+        self.exit_action = QAction(translation_service.tr("menu_exit"), self)
         self.file_menu.addAction(self.exit_action)
         self.addMenu(self.file_menu)
 
         # Menú Operativo
-        self.operativo_menu = QMenu(translation_service.tr("ops_menu"), self)
-        self.ops_new_action = QAction(translation_service.tr("new"), self)
-        self.ops_open_action = QAction(translation_service.tr("open"), self)
-        self.ops_export_action = QAction(translation_service.tr("export"), self)
-        self.ops_close_action = QAction(translation_service.tr("close"), self)
+        self.operativo_menu = QMenu(translation_service.tr("menu_ops_menu"), self)
+        self.ops_new_action = QAction(translation_service.tr("menu_new"), self)
+        self.ops_open_action = QAction(translation_service.tr("menu_open"), self)
+        self.ops_export_action = QAction(translation_service.tr("menu_export"), self)
+        self.ops_close_action = QAction(translation_service.tr("menu_close"), self)
         self.operativo_menu.addAction(self.ops_new_action)
         self.operativo_menu.addAction(self.ops_open_action)
         self.operativo_menu.addAction(self.ops_export_action)
@@ -67,11 +69,13 @@ class MainMenuBar(QMenuBar):
         self.addMenu(self.operativo_menu)
 
         # Menú Concurso
-        self.concurso_menu = QMenu(translation_service.tr("contest_menu"), self)
-        self.contest_new_action = QAction(translation_service.tr("new"), self)
-        self.contest_open_action = QAction(translation_service.tr("open"), self)
-        self.contest_export_action = QAction(translation_service.tr("export"), self)
-        self.contest_close_action = QAction(translation_service.tr("close"), self)
+        self.concurso_menu = QMenu(translation_service.tr("menu_contest_menu"), self)
+        self.contest_new_action = QAction(translation_service.tr("menu_new"), self)
+        self.contest_open_action = QAction(translation_service.tr("menu_open"), self)
+        self.contest_export_action = QAction(
+            translation_service.tr("menu_export"), self
+        )
+        self.contest_close_action = QAction(translation_service.tr("menu_close"), self)
         self.concurso_menu.addAction(self.contest_new_action)
         self.concurso_menu.addAction(self.contest_open_action)
         self.concurso_menu.addAction(self.contest_export_action)
@@ -79,13 +83,17 @@ class MainMenuBar(QMenuBar):
         self.addMenu(self.concurso_menu)
 
         # Menú Base de datos
-        self.database_menu = QMenu(translation_service.tr("database_menu"), self)
+        self.database_menu = QMenu(translation_service.tr("menu_database_menu"), self)
         self.db_import_pdf_action = QAction(
-            translation_service.tr("import_from_pdf"), self
+            translation_service.tr("menu_import_from_pdf"), self
         )
-        self.db_export_action = QAction(translation_service.tr("export"), self)
-        self.db_show_action = QAction(translation_service.tr("show_database"), self)
-        self.db_delete_action = QAction(translation_service.tr("delete_database"), self)
+        self.db_export_action = QAction(translation_service.tr("menu_export"), self)
+        self.db_show_action = QAction(
+            translation_service.tr("menu_show_database"), self
+        )
+        self.db_delete_action = QAction(
+            translation_service.tr("menu_delete_database"), self
+        )
         self.database_menu.addAction(self.db_import_pdf_action)
         self.database_menu.addAction(self.db_export_action)
         self.database_menu.addAction(self.db_show_action)
@@ -96,9 +104,13 @@ class MainMenuBar(QMenuBar):
         self.addMenu(self.database_menu)
 
         # Menú Aspecto
-        self.aspect_menu = QMenu(translation_service.tr("aspect_menu"), self)
-        self.light_theme_action = QAction(translation_service.tr("light_theme"), self)
-        self.dark_theme_action = QAction(translation_service.tr("dark_theme"), self)
+        self.aspect_menu = QMenu(translation_service.tr("menu_aspect_menu"), self)
+        self.light_theme_action = QAction(
+            translation_service.tr("menu_light_theme"), self
+        )
+        self.dark_theme_action = QAction(
+            translation_service.tr("menu_dark_theme"), self
+        )
         self.light_theme_action.setCheckable(True)
         self.dark_theme_action.setCheckable(True)
         self.aspect_menu.addAction(self.light_theme_action)
@@ -106,9 +118,9 @@ class MainMenuBar(QMenuBar):
         self.addMenu(self.aspect_menu)
 
         # Menú Idioma
-        self.language_menu = QMenu(translation_service.tr("language_menu"), self)
-        self.lang_es_action = QAction(translation_service.tr("spanish"), self)
-        self.lang_en_action = QAction(translation_service.tr("english"), self)
+        self.language_menu = QMenu(translation_service.tr("menu_language_menu"), self)
+        self.lang_es_action = QAction(translation_service.tr("menu_spanish"), self)
+        self.lang_en_action = QAction(translation_service.tr("menu_english"), self)
         self.lang_es_action.setCheckable(True)
         self.lang_en_action.setCheckable(True)
         self.language_menu.addAction(self.lang_es_action)
@@ -116,8 +128,8 @@ class MainMenuBar(QMenuBar):
         self.addMenu(self.language_menu)
 
         # Menú Ayuda
-        self.help_menu = QMenu(translation_service.tr("help_menu"), self)
-        self.about_action = QAction(translation_service.tr("about"), self)
+        self.help_menu = QMenu(translation_service.tr("menu_help_menu"), self)
+        self.about_action = QAction(translation_service.tr("menu_about"), self)
         self.help_menu.addAction(self.about_action)
         self.addMenu(self.help_menu)
 
@@ -149,35 +161,38 @@ class MainMenuBar(QMenuBar):
         """
         Actualiza los textos de todos los menús y acciones según el idioma actual.
         """
-        self.file_menu.setTitle(translation_service.tr("file_menu"))
-        self.open_folder_action.setText(translation_service.tr("open_folder"))
-        self.exit_action.setText(translation_service.tr("exit"))
 
-        self.operativo_menu.setTitle(translation_service.tr("ops_menu"))
-        self.ops_new_action.setText(translation_service.tr("new"))
-        self.ops_open_action.setText(translation_service.tr("open"))
-        self.ops_export_action.setText(translation_service.tr("export"))
-        self.ops_close_action.setText(translation_service.tr("close"))
+        self.file_menu.setTitle(translation_service.tr("menu_file_menu"))
+        self.open_folder_action.setText(translation_service.tr("menu_open_folder"))
+        self.exit_action.setText(translation_service.tr("menu_exit"))
 
-        self.concurso_menu.setTitle(translation_service.tr("contest_menu"))
-        self.contest_new_action.setText(translation_service.tr("new"))
-        self.contest_open_action.setText(translation_service.tr("open"))
-        self.contest_export_action.setText(translation_service.tr("export"))
-        self.contest_close_action.setText(translation_service.tr("close"))
+        self.operativo_menu.setTitle(translation_service.tr("menu_ops_menu"))
+        self.ops_new_action.setText(translation_service.tr("menu_new"))
+        self.ops_open_action.setText(translation_service.tr("menu_open"))
+        self.ops_export_action.setText(translation_service.tr("menu_export"))
+        self.ops_close_action.setText(translation_service.tr("menu_close"))
 
-        self.database_menu.setTitle(translation_service.tr("database_menu"))
-        self.db_import_pdf_action.setText(translation_service.tr("import_from_pdf"))
-        self.db_export_action.setText(translation_service.tr("export"))
-        self.db_show_action.setText(translation_service.tr("show_database"))
-        self.db_delete_action.setText(translation_service.tr("delete_database"))
+        self.concurso_menu.setTitle(translation_service.tr("menu_contest_menu"))
+        self.contest_new_action.setText(translation_service.tr("menu_new"))
+        self.contest_open_action.setText(translation_service.tr("menu_open"))
+        self.contest_export_action.setText(translation_service.tr("menu_export"))
+        self.contest_close_action.setText(translation_service.tr("menu_close"))
 
-        self.aspect_menu.setTitle(translation_service.tr("aspect_menu"))
-        self.light_theme_action.setText(translation_service.tr("light_theme"))
-        self.dark_theme_action.setText(translation_service.tr("dark_theme"))
+        self.database_menu.setTitle(translation_service.tr("menu_database_menu"))
+        self.db_import_pdf_action.setText(
+            translation_service.tr("menu_import_from_pdf")
+        )
+        self.db_export_action.setText(translation_service.tr("menu_export"))
+        self.db_show_action.setText(translation_service.tr("menu_show_database"))
+        self.db_delete_action.setText(translation_service.tr("menu_delete_database"))
 
-        self.language_menu.setTitle(translation_service.tr("language_menu"))
-        self.lang_es_action.setText(translation_service.tr("spanish"))
-        self.lang_en_action.setText(translation_service.tr("english"))
+        self.aspect_menu.setTitle(translation_service.tr("menu_aspect_menu"))
+        self.light_theme_action.setText(translation_service.tr("menu_light_theme"))
+        self.dark_theme_action.setText(translation_service.tr("menu_dark_theme"))
 
-        self.help_menu.setTitle(translation_service.tr("help_menu"))
-        self.about_action.setText(translation_service.tr("about"))
+        self.language_menu.setTitle(translation_service.tr("menu_language_menu"))
+        self.lang_es_action.setText(translation_service.tr("menu_spanish"))
+        self.lang_en_action.setText(translation_service.tr("menu_english"))
+
+        self.help_menu.setTitle(translation_service.tr("menu_help_menu"))
+        self.about_action.setText(translation_service.tr("menu_about"))
