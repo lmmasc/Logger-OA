1b3cead91c4c8a70ef7ec0da3f56a90319991e1f
lmmasc <lmmasc51@gmail.com>
2025-09-09 22:50:03 -0500

    ClockWidget: reduce tamaño de fuente a 20 puntos en label, reloj y fecha para mejor compacidad visual.

--- src/interface_adapters/ui/views/clock_widget.py
+++ src/interface_adapters/ui/views/clock_widget.py
@@ -9,7 +9,7 @@ class ClockWidget(QWidget):
         super().__init__(parent)
         self.utc = utc
         layout = QHBoxLayout()
-        layout.setSpacing(8)
+        layout.setSpacing(4)
         layout.setContentsMargins(0, 0, 0, 0)
         self.label = QLabel(label_text)
         self.label.setAlignment(Qt.AlignCenter)
@@ -19,7 +19,7 @@ class ClockWidget(QWidget):
             self.label.setObjectName("ClockLabelUTC")
         font_label = QFont()
         font_label.setBold(True)
-        font_label.setPointSize(12)
+        font_label.setPointSize(20)
         self.label.setFont(font_label)
         self.time = QLabel()
         self.time.setAlignment(Qt.AlignCenter)
@@ -29,7 +29,7 @@ class ClockWidget(QWidget):
             self.time.setObjectName("ClockTimeUTC")
         font_time = QFont()
         font_time.setBold(True)
-        font_time.setPointSize(30)
+        font_time.setPointSize(20)
         self.time.setFont(font_time)
         self.date = QLabel()
         self.date.setAlignment(Qt.AlignCenter)
@@ -38,7 +38,7 @@ class ClockWidget(QWidget):
         else:
             self.date.setObjectName("ClockDateUTC")
         font_date = QFont()
-        font_date.setPointSize(12)
+        font_date.setPointSize(20)
         self.date.setFont(font_date)
         layout.addWidget(self.label)
         layout.addWidget(self.time)
@@ -64,6 +64,8 @@ class ClockWidget(QWidget):
         date_fmt = "%d/%m/%Y" if lang == "es" else "%m/%d/%Y"
         self.time.setText(now.strftime("%H:%M:%S"))
         self.date.setText(now.strftime(date_fmt))
+        # Asignar texto del label: solo "OA" o "UTC"
+        self.label.setText("OA" if not self.utc else "UTC")
 
     def set_label_text(self, text):
         self.label.setText(text)
